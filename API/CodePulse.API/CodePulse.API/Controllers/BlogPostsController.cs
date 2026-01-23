a1457eb19063e75ddee8839585773f12bb325679
Vaibhav2596 <chavanv214@gmail.com>
2026-01-23 22:26:02 +0530

    API - Change GetAll BlogPosts Action Method - Return Categories

--- API/CodePulse.API/CodePulse.API/Controllers/BlogPostsController.cs
+++ API/CodePulse.API/CodePulse.API/Controllers/BlogPostsController.cs
@@ -90,7 +90,13 @@ namespace CodePulse.API.Controllers
                     UrlHandle = blogPost.UrlHandle,
                     PublishedDate = blogPost.PublishedDate,
                     Author = blogPost.Author,
-                    IsVisible = blogPost.IsVisible
+                    IsVisible = blogPost.IsVisible,
+                    Categories = blogPost.Categories.Select(x => new CategoryDto
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        UrlHandle = x.UrlHandle
+                    }).ToList()
                 });
             }
 
