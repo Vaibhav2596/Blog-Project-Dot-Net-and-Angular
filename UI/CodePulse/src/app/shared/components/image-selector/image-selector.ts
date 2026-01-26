690646868dbe30b22fcbf6ed2bf3910239a03564
Vaibhav2596 <chavanv214@gmail.com>
2026-01-26 07:48:43 +0530

    UI - Create Image Service and Upload Image Form

--- UI/CodePulse/src/app/shared/components/image-selector/image-selector.ts
+++ UI/CodePulse/src/app/shared/components/image-selector/image-selector.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ImageSelectorService } from '../../services/image-selector-service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-image-selector',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './image-selector.html',
   styleUrl: './image-selector.css',
 })
@@ -11,7 +12,49 @@ export class ImageSelector {
   private imageSelectorService = inject(ImageSelectorService);
   showImageSelector = this.imageSelectorService.showImageSelector.asReadonly();
 
-  hideImageSelector(){
-    this.imageSelectorService.hideImageSelector()
+  imageSelectorUploadForm = new FormGroup({
+    file: new FormControl<File | null | undefined>(null, {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    name: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required,Validators.maxLength(100)],
+    }),
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required,Validators.maxLength(100)],
+    }),
+  });
+
+  hideImageSelector() {
+    this.imageSelectorService.hideImageSelector();
+  }
+
+  onFileSelected(event:Event){
+    const input = event.target as HTMLInputElement;
+    if(!input.files || input.files.length === 0){
+      return;
+    }
+
+    const file = input.files[0];
+    this.imageSelectorUploadForm.patchValue({
+      file: file
+    });
+  }
+
+  onSubmit(){
+    if(this.imageSelectorUploadForm.valid){
+      const formRawValue = this.imageSelectorUploadForm.getRawValue();
+
+      this.imageSelectorService.uploadImage(formRawValue.file!,formRawValue.name,formRawValue.title).subscribe({
+        next: (response) => {
+          console.log(response)
+        },
+        error : () => { 
+          console.error('Something went wrong!')
+        }
+      })
+    }
   }
 }
--- UI/CodePulse/src/app/shared/services/image-selector-service.ts
+++ UI/CodePulse/src/app/shared/services/image-selector-service.ts
@@ -1,9 +1,14 @@
-import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogImage } from '../models/image.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ImageSelectorService {
+  http = inject(HttpClient);
   showImageSelector = signal<boolean>(false);
 
   displayImageSelector(){
@@ -12,5 +17,13 @@ export class ImageSelectorService {
 
   hideImageSelector(){
     this.showImageSelector.set(false);
+  }
+
+  uploadImage(file:File,fileName:string,title:string):Observable<BlogImage>{
+    const formData = new FormData();
+    formData.append('file',file);
+    formData.append('fileName',fileName);
+    formData.append('title',title);
+    return this.http.post<BlogImage>(`${environment.apiBaseUrl}/api/images`,formData);
   }
 }
